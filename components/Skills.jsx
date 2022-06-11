8a10065e3d3bb6c84dd1decdcba5ce580af943fb
Williams Amaya <whistemalo@gmail.com>
2022-06-10 23:54:38 -0600

    path fix skills

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
 import Image from 'next/image';
+import css from '../public/assets/skills/css.png'
+import firebase   from  '../public/assets/skills/firebase.png'
+import github1    from  '../public/assets/skills/github1.png' 
+import html       from  '../public/assets/skills/html.png' 
+import javascript from  '../public/assets/skills/javascript.png'
+import colab      from  '../public/assets/skills/colab.png' 
+import java       from  '../public/assets/skills/java.png' 
+import linux      from  '../public/assets/skills/linux.png' 
+import mongo      from  '../public/assets/skills/mongo.png' 
+import node       from  '../public/assets/skills/node.png' 
+import php        from  '../public/assets/skills/php.png' 
+import python     from  '../public/assets/skills/python.png' 
+import react      from  '../public/assets/skills/react.png' 
+import sql_server from  '../public/assets/skills/sql_server.png'
+import tailwind   from  '../public/assets/skills/tailwind.png' 
+
 
 function Skills() {
   return (
@@ -11,49 +27,49 @@ function Skills() {
             <div className='p-1 shadow-xl rounded-xl' >
                 <div className='py-10 p-3 grid grid-cols-5 gap-5 justify-center items-center'>
                     <div className='p-1 m-auto hover:scale-105 ease-in duration-300'> 
-                        <Image alt='/' src='/../public/skills/css.png' width='48px' height='64px'/>
+                        <Image alt="css" src={css} width='48px' height='64px'/>
                     </div>
                     <div className='p-1 m-auto hover:scale-105 ease-in duration-300'> 
-                        <Image alt='/' src='/../public/skills/firebase.png' width='64px' height='64px'/>
+                        <Image alt='/' src={firebase} width='64px' height='64px'/>
                     </div>
                     <div className='p-1 m-auto hover:scale-105 ease-in duration-300'> 
-                        <Image alt='/' src='/../public/skills/github1.png' width='64px' height='64px'/>
+                        <Image alt='/' src={github1} width='64px' height='64px'/>
                     </div>
                     <div className='p-1 m-auto hover:scale-105 ease-in duration-300'> 
-                        <Image alt='/' src='/../public/skills/html.png' width='48px' height='64px'/>
+                        <Image alt='/' src={html} width='48px' height='64px'/>
                     </div>
                     <div className='p-1 m-auto hover:scale-105 ease-in duration-300'> 
-                        <Image alt='/' src='/../public/skills/javascript.png' width='64px' height='64px'/>
+                        <Image alt='/' src={javascript} width='64px' height='64px'/>
                     </div>
                     <div className='p-1 m-auto hover:scale-105 ease-in duration-300'> 
-                        <Image alt='/' src='/../public/skills/colab.png' width='64px' height='64px'/>
+                        <Image alt='/' src={colab} width='64px' height='64px'/>
                     </div>
                     <div className='p-1 m-auto hover:scale-105 ease-in duration-300'> 
-                        <Image alt='/' src='/../public/skills/java.png' width='64px' height='64px'/>
+                        <Image alt='/' src={java} width='64px' height='64px'/>
                     </div>
                     <div className='p-1 m-auto hover:scale-105 ease-in duration-300'> 
-                        <Image alt='/' src='/../public/skills/linux.png' width='64px' height='64px'/>
+                        <Image alt='/' src={linux} width='64px' height='64px'/>
                     </div>
                     <div className='p-1 m-auto hover:scale-105 ease-in duration-300'> 
-                        <Image alt='/' src='/../public/skills/mongo.png' width='64px' height='64px'/>
+                        <Image alt='/' src={mongo} width='64px' height='64px'/>
                     </div>
                     <div className='p-1 m-auto hover:scale-105 ease-in duration-300'> 
-                        <Image alt='/' src='/../public/skills/node.png' width='64px' height='64px'/>
+                        <Image alt='/' src={node} width='64px' height='64px'/>
                     </div>
                     <div className='p-1 m-auto hover:scale-105 ease-in duration-300'> 
-                        <Image alt='/' src='/../public/skills/php.png' width='64px' height='64px'/>
+                        <Image alt='/' src={php} width='64px' height='64px'/>
                     </div>
                     <div className='p-1 m-auto hover:scale-105 ease-in duration-300'> 
-                        <Image alt='/' src='/../public/skills/python.png' width='64px' height='64px'/>
+                        <Image alt='/' src={python} width='64px' height='64px'/>
                     </div>
                     <div className='p-1 m-auto hover:scale-105 ease-in duration-300'> 
-                        <Image alt='/' src='/../public/skills/react.png' width='64px' height='64px'/>
+                        <Image alt='/' src={react} width='64px' height='64px'/>
                     </div>
                     <div className='p-1 m-auto hover:scale-105 ease-in duration-300'> 
-                        <Image alt='/' src='/../public/skills/sql-server.png' width='64px' height='64px'/>
+                        <Image alt='/' src={sql_server} width='64px' height='64px'/>
                     </div>
                     <div className='p-1 m-auto hover:scale-105 ease-in duration-300'> 
-                        <Image alt='/' src='/../public/skills/tailwind.png' width='64px' height='64px'/>
+                        <Image alt='/' src={tailwind} width='64px' height='64px'/>
                     </div>
                 </div>
 
