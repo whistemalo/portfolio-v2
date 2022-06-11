aaff7142690d638fa7c2faeec4a084bfd30a4cc4
Williams Amaya <whistemalo@gmail.com>
2022-06-10 23:34:06 -0600

    skills path fix

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -11,49 +11,49 @@ function Skills() {
             <div className='p-1 shadow-xl rounded-xl' >
                 <div className='py-10 p-3 grid grid-cols-5 gap-5 justify-center items-center'>
                     <div className='p-1 m-auto hover:scale-105 ease-in duration-300'> 
-                        <Image alt='/' src='/../public/assets/skills/css.png' width='48px' height='64px'/>
+                        <Image alt='/' src='/../public/skills/css.png' width='48px' height='64px'/>
                     </div>
                     <div className='p-1 m-auto hover:scale-105 ease-in duration-300'> 
-                        <Image alt='/' src='/../public/assets/skills/firebase.png' width='64px' height='64px'/>
+                        <Image alt='/' src='/../public/skills/firebase.png' width='64px' height='64px'/>
                     </div>
                     <div className='p-1 m-auto hover:scale-105 ease-in duration-300'> 
-                        <Image alt='/' src='/../public/assets/skills/github1.png' width='64px' height='64px'/>
+                        <Image alt='/' src='/../public/skills/github1.png' width='64px' height='64px'/>
                     </div>
                     <div className='p-1 m-auto hover:scale-105 ease-in duration-300'> 
-                        <Image alt='/' src='/../public/assets/skills/html.png' width='48px' height='64px'/>
+                        <Image alt='/' src='/../public/skills/html.png' width='48px' height='64px'/>
                     </div>
                     <div className='p-1 m-auto hover:scale-105 ease-in duration-300'> 
-                        <Image alt='/' src='/../public/assets/skills/javascript.png' width='64px' height='64px'/>
+                        <Image alt='/' src='/../public/skills/javascript.png' width='64px' height='64px'/>
                     </div>
                     <div className='p-1 m-auto hover:scale-105 ease-in duration-300'> 
-                        <Image alt='/' src='/../public/assets/skills/colab.png' width='64px' height='64px'/>
+                        <Image alt='/' src='/../public/skills/colab.png' width='64px' height='64px'/>
                     </div>
                     <div className='p-1 m-auto hover:scale-105 ease-in duration-300'> 
-                        <Image alt='/' src='/../public/assets/skills/java.png' width='64px' height='64px'/>
+                        <Image alt='/' src='/../public/skills/java.png' width='64px' height='64px'/>
                     </div>
                     <div className='p-1 m-auto hover:scale-105 ease-in duration-300'> 
-                        <Image alt='/' src='/../public/assets/skills/linux.png' width='64px' height='64px'/>
+                        <Image alt='/' src='/../public/skills/linux.png' width='64px' height='64px'/>
                     </div>
                     <div className='p-1 m-auto hover:scale-105 ease-in duration-300'> 
-                        <Image alt='/' src='/../public/assets/skills/mongo.png' width='64px' height='64px'/>
+                        <Image alt='/' src='/../public/skills/mongo.png' width='64px' height='64px'/>
                     </div>
                     <div className='p-1 m-auto hover:scale-105 ease-in duration-300'> 
-                        <Image alt='/' src='/../public/assets/skills/node.png' width='64px' height='64px'/>
+                        <Image alt='/' src='/../public/skills/node.png' width='64px' height='64px'/>
                     </div>
                     <div className='p-1 m-auto hover:scale-105 ease-in duration-300'> 
-                        <Image alt='/' src='/../public/assets/skills/php.png' width='64px' height='64px'/>
+                        <Image alt='/' src='/../public/skills/php.png' width='64px' height='64px'/>
                     </div>
                     <div className='p-1 m-auto hover:scale-105 ease-in duration-300'> 
-                        <Image alt='/' src='/../public/assets/skills/python.png' width='64px' height='64px'/>
+                        <Image alt='/' src='/../public/skills/python.png' width='64px' height='64px'/>
                     </div>
                     <div className='p-1 m-auto hover:scale-105 ease-in duration-300'> 
-                        <Image alt='/' src='/../public/assets/skills/react.png' width='64px' height='64px'/>
+                        <Image alt='/' src='/../public/skills/react.png' width='64px' height='64px'/>
                     </div>
                     <div className='p-1 m-auto hover:scale-105 ease-in duration-300'> 
-                        <Image alt='/' src='/../public/assets/skills/sql-server.png' width='64px' height='64px'/>
+                        <Image alt='/' src='/../public/skills/sql-server.png' width='64px' height='64px'/>
                     </div>
                     <div className='p-1 m-auto hover:scale-105 ease-in duration-300'> 
-                        <Image alt='/' src='/../public/assets/skills/tailwind.png' width='64px' height='64px'/>
+                        <Image alt='/' src='/../public/skills/tailwind.png' width='64px' height='64px'/>
                     </div>
                 </div>
 
